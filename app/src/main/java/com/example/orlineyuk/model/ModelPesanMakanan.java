b520366a590d7492e6bbeb23b13bdec92d2fe696
alfincahyo <38088162+alfincahyo@users.noreply.github.com>
2019-05-11 01:11:33 +0700

    Update model pesanan
    sedikit perubahan nama variabel

--- app/src/main/java/com/example/orlineyuk/model/ModelPesanMakanan.java
+++ app/src/main/java/com/example/orlineyuk/model/ModelPesanMakanan.java
@@ -2,30 +2,45 @@ package com.example.orlineyuk.model;
 
 public class ModelPesanMakanan {
 
-    String nama;
-    String harga;
-    String Jumlah;
+    private String id;
+    private String nama;
+    private String email;
+    private Integer harga;
+    private Integer jumpesanan;
+    private String StatPesan;
 
-    public ModelPesanMakanan(){
+    public ModelPesanMakanan(){}
 
-    }
-
-
-    public ModelPesanMakanan(String nama, String harga, String jumlah) {
+    public ModelPesanMakanan(String id, String nama, Integer jumpesanan, Integer harga, String statPesan, String email) {
+        this.id = id;
         this.nama = nama;
+        this.email = email;
         this.harga = harga;
-        Jumlah = jumlah;
+        this.jumpesanan = jumpesanan;
+        StatPesan = statPesan;
     }
 
     public String getNama() {
         return nama;
     }
 
-    public String getHarga() {
+    public Integer getHarga() {
         return harga;
     }
 
-    public String getJumlah() {
-        return Jumlah;
+    public Integer getJumpesanan() {
+        return jumpesanan;
+    }
+
+    public String getStatPesan() {
+        return StatPesan;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getId() {
+        return id;
     }
 }
